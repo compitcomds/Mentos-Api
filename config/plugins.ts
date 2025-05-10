20edc98892b32d298d001bd335c61d1a97d9aced
DarshanJain <compitcomds@gmail.com>
2025-05-11 00:15:31 +0530

    update

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,18 +4,18 @@ module.exports = ({ env }) => ({
       enabled: true,
     },
     // config/plugins.{js,ts}
-'strapi-cache': {
-  enabled: true,
-  config: {
-    debug: false, // Enable debug logs
-    max: 1000, // Maximum number of items in the cache (only for memory cache)
-    ttl: 1000 * 60 * 60, // Time to live for cache items (1 hour)
-    size: 1024 * 1024 * 200, // Maximum size of the cache (1 GB) (only for memory cache)
-    allowStale: false, // Allow stale cache items (only for memory cache)
-    cacheableRoutes: ['/api/blogs', '/api/web-medias'], // Caches routes which start with these paths (if empty array, all '/api' routes are cached)
-    provider: 'memory', // Cache provider ('memory' or 'redis')
-    redisUrl: env('REDIS_URL', 'redis://localhost:6379'), // Redis URL (if using Redis)
-  },
-},
+// 'strapi-cache': {
+//   enabled: true,
+//   config: {
+//     debug: false, // Enable debug logs
+//     max: 1000, // Maximum number of items in the cache (only for memory cache)
+//     ttl: 1000 * 60 * 60, // Time to live for cache items (1 hour)
+//     size: 1024 * 1024 * 200, // Maximum size of the cache (1 GB) (only for memory cache)
+//     allowStale: false, // Allow stale cache items (only for memory cache)
+//     cacheableRoutes: ['/api/blogs', '/api/web-medias'], // Caches routes which start with these paths (if empty array, all '/api' routes are cached)
+//     provider: 'memory', // Cache provider ('memory' or 'redis')
+//     redisUrl: env('REDIS_URL', 'redis://localhost:6379'), // Redis URL (if using Redis)
+//   },
+// },
     // ...
   });
